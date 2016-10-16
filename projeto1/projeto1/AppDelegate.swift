6afe8e82b34242bd8568de06081f6f0f5a0a2397
Hilton Pintor <hpbl@cin.ufpe.br>
2016-10-16 11:28:38 -0300

    creating xcode project

---
+++ projeto1/projeto1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  projeto1
+//
+//  Created by Hilton Pintor Bezerra Leite on 16/10/16.
+//  Copyright © 2016 Pintor&Chien. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ projeto1/projeto1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  projeto1
+//
+//  Created by Hilton Pintor Bezerra Leite on 16/10/16.
+//  Copyright © 2016 Pintor&Chien. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
